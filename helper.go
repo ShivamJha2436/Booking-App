f31e8c8162e2218e4d3b65a55dc647ab3fecaff9
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-08-27 11:19:35 +0530

    Initial commit

--- helper.go
+++ helper.go
@@ -1,11 +1,13 @@
-package main
+package Booking_App
 
-import "strings"
+import (
+	"strings"
+)
 
 // validateUserInput checks if the user input is valid
 func validateUserInput(firstName, lastName, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	isValidTicketNumber := userTickets > 0 && userTickets <= main_go.remainingTickets
 	return isValidName, isValidEmail, isValidTicketNumber
 }
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package Booking_App
 
 import (
 	"errors"
